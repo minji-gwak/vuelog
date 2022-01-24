2e0f5b3aca97d5911fbc49b6c7d74e8b97c4999e
곽민지 <mini0006@paiptree.com>
2022-01-24 16:33:20 +0900

    vuelog 2강까지

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-for="(blog, i) in blogData" :key="i">
+        <h5>{{ blog.title }}</h5>
+        <p>{{ blog.content }}</p>
+        <p>{{ blog.date }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'list',
+    props: [
+        'blogData'
+    ],
+    components: {
+
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
